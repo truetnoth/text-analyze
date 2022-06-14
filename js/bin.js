991a1df9d3234d95761771c7d4e12d4111747aa5
Valera Trusevich <truetnoth@gmail.com>
2022-06-14 18:23:42 +0300

    bug fixing

--- js/bin.js
+++ js/bin.js
@@ -8,8 +8,8 @@ const tonPlaceNumber = document.querySelector(".ton-number");
 
 const take_result = document.querySelector(".take_result");
 
-const countAllWords = (str) => { //считаем все слова в тексте
-    return str.trim().match(/\b\S+[^0-9]\b/gi);
+const countAllWords = (text) => { //считаем все слова в тексте
+    return text.trim().match(/\b\S+[^0-9]\b/gi);
 };
 
 const countWords = (text, vocabulary) => {
